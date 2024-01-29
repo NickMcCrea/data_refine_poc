3252436a8b5a4f01394e8785a637b5afe6869eb9
NickMcCrea <nickmccrea@gmail.com>
2024-01-29 20:57:38 +0000

    Refactor layout styles and update dependencies

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,8 +6,8 @@ import { useState, useEffect } from 'react';
 import Header from './components/Header/Header';
 import BasicTable from './components/BasicTable/BasicTable';
 import { DataRow } from './components/DataRow';
-import RestClient  from './services/RestClient';
-import SocketClient  from './services/SocketClient';
+import RestClient from './services/RestClient';
+import SocketClient from './services/SocketClient';
 import ChatInputWithSend from './components/ChatInput/ChatInputWithSend';
 import styles from './App.module.css';
 import { ConversationData } from './DataSetConversation';
@@ -18,22 +18,23 @@ function App() {
 
   //state to hold our websocket session_id
   const [sessionId, setSessionId] = useState<string | null>(null);
-
   const [conversationData, setConversationData] = useState<ConversationData | null>(null);
-  const restClient =  RestClient.getInstance();
+  const restClient = RestClient.getInstance();
   const socketClient = SocketClient.getInstance('http://localhost:5001');
+
+  const [layoutState, setLayoutState] = useState<string>('mainLayout');
+
 
   // Registering endpoints
   restClient.registerEndpoint('submit_query', { url: 'http://localhost:5001/submit_query', method: 'POST' });
 
 
-  
+
   // Connecting to the socket
   useEffect(() => {
-    socketClient.subscribe("submit_query_update", x => 
-    {
-        console.log('submit_query_update');
-        console.log(x);
+    socketClient.subscribe("submit_query_update", x => {
+      console.log('submit_query_update');
+      console.log(x);
     });
 
     socketClient.subscribe("connected", x => {
@@ -46,12 +47,12 @@ function App() {
 
       //store it
       setSessionId(sessionId);
-  });
+    });
 
   }, []);
 
-   // Function to handle chat input submission
-   const handleSubmit = (inputValue: string) => {
+  // Function to handle chat input submission
+  const handleSubmit = (inputValue: string) => {
 
     //createa JSON to send
     const json = {
@@ -66,29 +67,45 @@ function App() {
       .then(data => {
         console.log(data);
         setConversationData(data);
-    
+
       })
       .catch(error => {
-        
+
         console.error(error);
       });
   };
 
 
+  function renderLayout(state: string) {
+    switch (state) {
+      case 'initialLayout':
+        return (
+          <div className={styles.initialLayout}>
+            <div className={styles.header}><Header /></div>
+            <div className={styles.mainPanel}>
+              <ChatInputWithSend onSubmit={handleSubmit} />
+            </div>
+          </div>
+        );
+      case 'mainLayout':
+        return (
+          <div className={styles.mainLayout}>
+            <div className={styles.header}><Header /></div>
+            <div className={styles.sidebar}>  
+              <ChatInputWithSend onSubmit={handleSubmit} />
+            </div>
+          </div>
+        );
+      default:
+        return (<div />);
+    }
+  }
 
 
   return (
-    <div className={styles.layout2}>
-    <div className={styles.header}><Header/></div>
-    <div className={styles.mainPanel}>
-    <ChatInputWithSend onSubmit={handleSubmit} />
+    <div>
+      {renderLayout(layoutState)}
     </div>
-
-    {/* <div className="sidebar-placeholder">Sidebar</div>
-    <div className="footer-placeholder">Footer</div> */}
-  </div>
-
-
   );
 }
 
